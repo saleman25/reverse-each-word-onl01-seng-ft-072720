007aba9c1bdc4d2acb021305b4b92e4559345767
Samantha Aleman <samantha.aleman25@gmail.com>
2020-07-30 03:38:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
 sentence = sentence.split( )
 reverse.sentence= []
 sentence.collect do |word|
-  reversed.sentence << word.reversed
+  reversed.sentence << word.reverse
 end
 reversed.sentence.join(" ")
 end
